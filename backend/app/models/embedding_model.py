da7cad6801dcf6ed228107d92c7cf52f6615254f
ChriisLM <christianlezcano878@gmail.com>
2025-05-12 13:47:30 -0500

    feat: Create model for embedding

---
+++ backend/app/models/embedding_model.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from uuid import UUID
+
+class EmbeddingBase(BaseModel):
+  resource_id: UUID
+  embedding: list[float]
+
+class EmbeddingCreate(EmbeddingBase):
+  pass
+
+class EmbeddingOut(EmbeddingBase):
+  id: UUID
+  resource_id: UUID
+  
+  class Config:
+    orm_mode = True
