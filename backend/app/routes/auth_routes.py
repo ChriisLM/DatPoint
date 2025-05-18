2cf292ef5d04cf22b2cc1b43a5cb83b2513c7af2
ChriisLM <christianlezcano878@gmail.com>
2025-05-17 21:18:01 -0500

    feat: Create new route for auth endpoints, create register endpoint

---
+++ backend/app/routes/auth_routes.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, HTTPException, status
+from app.models.user_model import UserCreate, UserOut
+from app.services.user_services import create_user, get_user_by_email
+
+router = APIRouter(prefix="/auth", tags=["Authentication"])
+
+@router.post("/register", response_model=UserOut,status_code=status.HTTP_201_CREATED)
+async def register_user(user: UserCreate):
+  existing_user = await get_user_by_email(user.email)
+  if existing_user:
+    raise HTTPException(status_code=400, detail="Email already registered")
+  
+  new_user = await create_user(user)
+  return new_user
+
