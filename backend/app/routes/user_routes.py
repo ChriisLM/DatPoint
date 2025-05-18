04b58d35042f5406b7a837ca4ba8777d98bb2467
ChriisLM <christianlezcano878@gmail.com>
2025-05-17 21:16:45 -0500

    feat: Refactory endpoint, and create get_me for current user

--- backend/app/routes/user_routes.py
+++ backend/app/routes/user_routes.py
@@ -1,14 +1,9 @@
-from fastapi import APIRouter, HTTPException, status
-from app.models.user_model import UserCreate, UserOut
-from app.services.user_services import create_user, get_user_by_email
+from fastapi import APIRouter, Depends
+from app.models.user_model import UserBase, UserOut
+from backend.app.services.auth_services import get_current_user
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post("/register", response_model=UserOut,status_code=status.HTTP_201_CREATED)
-async def register_user(user: UserCreate):
-  existing_user = await get_user_by_email(user.email)
-  if existing_user:
-    raise HTTPException(status_code=400, detail="Email already registered")
-  
-  new_user = await create_user(user)
-  return new_user
+@router.post("/me", response_model=UserOut)
+async def get_me(current_user: UserBase = Depends(get_current_user)):
+    return current_user
