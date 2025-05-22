fede4dce832735381a0f19fa3d020a34713b350f
ChriisLM <christianlezcano878@gmail.com>
2025-05-21 20:05:59 -0500

    feat: Add endpoint to get the list of resources of the current user

--- backend/app/routes/resource_routes.py
+++ backend/app/routes/resource_routes.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter, HTTPException, status
+from typing import List
+from fastapi import APIRouter, Depends, HTTPException, status
 from app.models.resource_model import ResourceCreate, ResourceOut
-from app.services.resource_services import create_resource, get_resource_by_id
+from app.services.resource_services import create_resource, get_resource_by_id, list_resources_by_user
 from uuid import UUID
+
+from app.models.user_model import UserBase
+from app.services.auth_services import get_current_user
 
 router = APIRouter(prefix="/resources", tags=["Resources"])
 
@@ -9,6 +13,11 @@ router = APIRouter(prefix="/resources", tags=["Resources"])
 async def create_new_resource(resource: ResourceCreate):
     new_resource = await create_resource(resource)
     return new_resource
+
+@router.get("/me", response_model=List[ResourceOut])
+async def get_resources_by_me(current_user: UserBase = Depends(get_current_user)):
+    list_resources = await list_resources_by_user(current_user)
+    return list_resources
 
 @router.get("/{resource_id}", response_model=ResourceOut)
 async def get_resource(resource_id: UUID):
