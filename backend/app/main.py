46f5990fe63f495b03371133afcc416d4d1eb76f
ChriisLM <christianlezcano878@gmail.com>
2025-05-26 11:50:06 -0500

    feat: Update routes prefix and add auth routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routes import user_routes, resource_routes
+from app.routes import auth_routes, resource_routes, user_routes
 from app.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
@@ -7,7 +7,6 @@ app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 # Configurar CORS
 # setup_cors(app)
 
-# Incluir rutas
-# app.include_router(auth_routes.router, prefix="/auth", tags=["Authentication"])
-app.include_router(user_routes.router, prefix="/users", tags=["Users"])
-app.include_router(resource_routes.router, prefix="/resource", tags=["Resources"])
+app.include_router(auth_routes.router)
+app.include_router(user_routes.router)
+app.include_router(resource_routes.router)
