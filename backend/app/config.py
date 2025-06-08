abfc55e45295b21e26c9774aec3116f34d2a26e4
ChriisLM <christianlezcano878@gmail.com>
2025-06-08 12:02:08 -0500

    feat: Extend the expiration time of the jwt

--- backend/app/config.py
+++ backend/app/config.py
@@ -15,6 +15,6 @@ class Settings(BaseSettings):
     # Seguridad
     SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
 settings = Settings()
