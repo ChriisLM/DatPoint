08be619c7b41571839e4ad03856658a1917b2b1e
ChriisLM <christianlezcano878@gmail.com>
2025-06-23 22:04:58 -0500

    feat: Create sidebar nav component

---
+++ frontend/web/src/app/features/dashboard/components/sidebar/sidebar-navegation/sidebar-navegation.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { IconsModule } from '../../../../../shared/icons/icons.module';
+
+@Component({
+  selector: 'dtp-sidebar-navegation',
+  imports: [IconsModule],
+  templateUrl: './sidebar-navegation.component.html',
+})
+export class SidebarNavegationComponent { }
