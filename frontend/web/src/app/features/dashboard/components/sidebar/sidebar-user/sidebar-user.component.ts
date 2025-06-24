947342698bbe667f5e913c00013d1fbf1397f32c
ChriisLM <christianlezcano878@gmail.com>
2025-06-23 22:05:36 -0500

    feat: Create component for display user and settings

---
+++ frontend/web/src/app/features/dashboard/components/sidebar/sidebar-user/sidebar-user.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { IconsModule } from '../../../../../shared/icons/icons.module';
+
+@Component({
+  selector: 'dtp-sidebar-user',
+  imports: [IconsModule],
+  templateUrl: './sidebar-user.component.html',
+})
+export class SidebarUserComponent { }
