b6a34fabf0ee2c0122460b81a7516502682acf3b
ChriisLM <christianlezcano878@gmail.com>
2025-07-15 12:28:39 -0500

    feat: Implements routes for sidebard dashboard and login / resgister

--- frontend/web/src/app/app.routes.ts
+++ frontend/web/src/app/app.routes.ts
@@ -1,18 +1,27 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full'
+  path: '',
+  loadComponent: () => import('./pages/landing-page/landing-page.component'),
   },
   {
     path: 'dashboard',
-    component: DashboardComponent
+    loadComponent: () => import('./features/dashboard/dashboard.component'),
+    children: [
+      { path: '', loadComponent: () => import('./features/dashboard/pages/home-page/home-page.component')},
+      { path: 'recent', loadComponent: () => import('./features/dashboard/pages/recent-page/recent-page.component')},
+      { path: 'favorite', loadComponent: () => import('./features/dashboard/pages/favorite-page/favorite-page.component')},
+      { path: 'search', loadComponent: () => import('./features/dashboard/pages/result-search-page/result-search-page.component')},
+      { path: 'type/:type', loadComponent: () => import('./features/dashboard/pages/resource-list/resource-list.component')
+      },
+      { path: '**', redirectTo: ''}
+    ]
   },
+  { path: 'login', loadComponent: () => import('./pages/login-page/login-page.component') },
+  { path: 'register', loadComponent: () => import('./pages/register-page/register-page.component') },
   {
     path: '**',
-    redirectTo: 'dashboard'
+    redirectTo: ''
   }
 ];
