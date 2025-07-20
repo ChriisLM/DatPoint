80c63b901ff45d820e06011ec9d246586fc4a42b
ChriisLM <christianlezcano878@gmail.com>
2025-07-19 21:29:00 -0500

    feat: Add functionality to display modal new resource

--- frontend/web/src/app/shared/components/add-button/add-button.component.ts
+++ frontend/web/src/app/shared/components/add-button/add-button.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { IconsModule } from '../../icons/icons.module';
+import { DialogStateService } from '../../../features/dashboard/services/DialogStateService.service';
+
 
 @Component({
   selector: 'dtp-add-button',
@@ -9,4 +11,9 @@ import { IconsModule } from '../../icons/icons.module';
 export class AddButtonComponent { 
   @Input() label = 'Add';
   @Input() icon = 'Plus';
+
+  constructor(private modalService: DialogStateService) {}
+  openDialog() {
+    this.modalService.setModalOpen(true);
+  }
 }
