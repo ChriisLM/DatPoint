6110d912dbd2175cbca67c646a9e532dbbf264fe
ChriisLM <christianlezcano878@gmail.com>
2025-07-19 21:32:13 -0500

    feat: Add to dashboard modal new resource component

--- frontend/web/src/app/features/dashboard/dashboard.component.ts
+++ frontend/web/src/app/features/dashboard/dashboard.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { RouterOutlet } from '@angular/router';
+import { AddResourceDialogComponent } from "./components/add-resource-dialog/add-resource-dialog.component";
+import { DialogStateService } from './services/DialogStateService.service';
 
 @Component({
   selector: 'dtp-dashboard',
-  imports: [SidebarComponent, RouterOutlet],
+  imports: [SidebarComponent, RouterOutlet, AddResourceDialogComponent],
   templateUrl: './dashboard.component.html',
 })
-export default class DashboardComponent { }
+export default class DashboardComponent { 
+  visible = false;
+
+  constructor(private modalService: DialogStateService) {}
+
+  ngOnInit() {
+    this.modalService.modalOpen$.subscribe(state => {
+      this.visible = state;
+    });
+  }
+
+  close() {
+    this.modalService.setModalOpen(false);
+  }
+}
