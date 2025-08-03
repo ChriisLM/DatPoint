0e328437b9c3080f451c544085753fb7dd498f3c
ChriisLM <christianlezcano878@gmail.com>
2025-08-03 18:54:46 -0500

    feat: Add new icons

--- frontend/web/src/app/shared/icons/icons.module.ts
+++ frontend/web/src/app/shared/icons/icons.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { LucideAngularModule, Home, Clock, Star, FileText, Image as ImageIcon, Video, Link as LinkIcon, Plus, DatabaseZap, FolderOpen, Settings, LoaderCircle, Search, Grid2X2, List, GalleryVerticalEnd, Menu, ShieldCheck, Zap, RefreshCcw, Check, Calendar, Eye, Pencil, StarOff, Trash } from 'lucide-angular';
+import { LucideAngularModule, Home, Clock, Star, FileText, Image as ImageIcon, Video, Link as LinkIcon, Plus, DatabaseZap, FolderOpen, Settings, LoaderCircle, Search, Grid2X2, List, GalleryVerticalEnd, Menu, ShieldCheck, Zap, RefreshCcw, Check, Calendar, Eye, Pencil, StarOff, Trash, UserCircle, BadgeQuestionMark, LogOut } from 'lucide-angular';
 
 @NgModule({
   imports: [
@@ -29,7 +29,10 @@ import { LucideAngularModule, Home, Clock, Star, FileText, Image as ImageIcon, V
       Eye,
       Pencil,
       StarOff,
-      Trash
+      Trash,
+      UserCircle,
+      BadgeQuestionMark,
+      LogOut
     })
   ],
   exports: [LucideAngularModule]
