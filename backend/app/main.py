0b144aa0978ff5c2c6f3b78de7ebe10616ca8a39
ChriisLM <christianlezcano878@gmail.com>
2025-08-06 19:58:22 -0500

    feat: Add cors with local host for test

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.config import settings
 from app.routes import auth_routes, resource_routes, search_routes, user_routes
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
 
-# Configurar CORS
-# setup_cors(app)
+#CORS
+origins = [
+  "http://localhost:4200"
+]
 
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+#Rutas
 app.include_router(auth_routes.router)
 app.include_router(user_routes.router)
 app.include_router(resource_routes.router)
